cc01e6963ff5687dc7874f8c4a408e222d24f895
SahilMahadwar <accounts@mahadwar.com>
2024-09-27 17:03:58 +0530

    add layout

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,21 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 
 //Layouts
-// import { AuthenticationGuard } from "../components/auth/authentication-guard";
-// import { DashboardLayout } from "../layouts/dashboard-layout";
+import { DashboardLayout } from "../layouts/dashboard-layout";
+
+// Pages
 import AskPage from "../pages/ask";
 import { HomePage } from "../pages/home-page";
 import ProjectPage from "../pages/project-page";
 
-// Pages
-
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/project" element={<ProjectPage />} />
-      <Route path="/ask" element={<AskPage />} />
-      <Route path="/*" element={<div>404</div>} />
+      <Route element={<DashboardLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/project" element={<ProjectPage />} />
+        <Route path="/ask" element={<AskPage />} />
+        <Route path="/*" element={<div>404</div>} />{" "}
+      </Route>
     </Routes>
   );
 };
