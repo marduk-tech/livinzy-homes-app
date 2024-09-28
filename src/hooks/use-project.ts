fb4ebf1248fe7a477969978c31546db06388108b
SahilMahadwar <accounts@mahadwar.com>
2024-09-28 17:53:55 +0530

    update use-project hook

--- src/hooks/use-project.ts
+++ src/hooks/use-project.ts
@@ -3,19 +3,33 @@
 import { useQuery } from "@tanstack/react-query";
 import { axiosApiInstance } from "../libs/axios-api-Instance";
 import { queryKeys } from "../libs/constants";
+import { Project } from "../types/Project";
 
 /**
- * Custom hook to fetch project by id
- * @param {string} id - The ID of the project
- * @returns {UseQueryResult<Project, Error>} The result of the useQuery hook
+ * Custom hook to fetch all projects
+ * @returns {UseQueryResult<Project[], Error>} The result of the useQuery hook containing an array of projects
  */
-export const useFetchProject = (id?: string) => {
-  return useQuery<any, Error>({
-    queryKey: id ? [queryKeys.getProjectById, id] : [queryKeys.projects],
+export const useFetchProjects = () => {
+  return useQuery<Project[], Error>({
+    queryKey: [queryKeys.projects],
     queryFn: async () => {
-      const endpoint = id ? `/projects/${id}` : "/projects";
-      const { data } = await axiosApiInstance.get(endpoint);
+      const { data } = await axiosApiInstance.get("/projects");
       return data;
+    },
+  });
+};
+
+/**
+ * Custom hook to fetch a single project by its ID
+ * @param {string} id - The ID of the project to fetch
+ * @returns {UseQueryResult<Project, Error>} The result of the useQuery hook containing a single project
+ */
+export const useFetchProjectById = (id: string) => {
+  return useQuery<Project, Error>({
+    queryKey: [queryKeys.getProjectById, id],
+    queryFn: async () => {
+      const { data } = await axiosApiInstance.get(`/projects/${id}`);
+      return data as Project;
     },
   });
 };
