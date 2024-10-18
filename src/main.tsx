437f6650390f0ae390f1f281718511322eabe7ea
infinity <aseemagarwal19@gmail.com>
2024-10-18 12:03:26 +0530

    adding dynamic icons

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { LoadScriptNext } from "@react-google-maps/api";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
