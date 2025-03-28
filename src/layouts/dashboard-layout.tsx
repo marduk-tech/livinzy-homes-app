de4757b3e819cb3c2c17801569a9c36ce90c11d2
infinity <aseemagarwal19@gmail.com>
2025-03-28 09:34:19 +0530

    reverting logo & name

--- src/layouts/dashboard-layout.tsx
+++ src/layouts/dashboard-layout.tsx
@@ -150,7 +150,7 @@ export const DashboardLayout: React.FC = () => {
                 style={{ height: 60, display: "flex", alignItems: "center" }}
               >
                 <img
-                  src="/images/brickalytics-logo.png"
+                  src="/logo-name.png"
                   style={{ height: 28, width: "auto", marginLeft: 0 }}
                 ></img>
               </Flex>
